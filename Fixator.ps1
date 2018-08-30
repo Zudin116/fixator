e8361de2e622796de2ed84996d221d82e8350f2c
Zudin116 <Zudin116@gmail.com>
2018-08-30 23:15:59 +0300

    First commit

---
+++ Fixator.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host 'Добро пожаловать в программу контороля целостности "Фиксатор"' 
+$proj = Read-Host "Введите название проекта"
+$dir = $($MyInvocation.MyCommand.Path | Split-Path -Parent)
+Write-Host "Выберете ПО для контроля:"
+Write-Host "1 - Secret Net Studio - C (Версия 8.2.1156)"
+Write-host "2 - Kaspersky"
+$a = Read-Host "1-2"
+if ($a -eq 1) {& $dir\Isx_Sost.exe "#1" "&f1=$dir\lists\sns82.tre &f2=$dir\$proj &u1 &beep"}
+elseif ($a -eq 2) {& $dir\Isx_Sost.exe "#1" "&f1=C:\Program Files (x86)\Kaspersky Lab\ &f2=$dir\$proj &u4 &beep &filtr=*.exe;*.dll;*.ppl;*.sys"}
+else {Write-Host "Некорректное значение"}
+Write-Host "Работа программы завершена."
+Read-Host "Нажмите Enter для выхода"
